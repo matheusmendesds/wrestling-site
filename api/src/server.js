d499c00bcdfcbcae827495eac49b01da734f0b73
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2024-08-06 15:34:47 -0300

    att

--- api/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js'
-
-const PORT = process.env.PORT || 3001
-
-// escutar a porta 3000
-app.listen(PORT, ()=>{
-    console.log(`o servidor esta rodando no endereço http://localhost:${PORT}`)
-})
